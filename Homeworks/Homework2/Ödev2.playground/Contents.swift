3993bba9ace835e72ebc0928cb308afd0b654e59
Selin <selinnn_32@hotmail.com>
2022-04-18 00:06:20 +0300

    homework2 added

---
+++ Homeworks/Homework2/Ödev2.playground/Contents.swift
@@ -0,0 +1,93 @@
+import UIKit
+
+class Soru1{
+    func dereceFahrenhiet(derece:Double) -> Double {
+        let fahrenhiet = (derece * 1.8) + 32
+        return fahrenhiet
+    }
+    
+}
+
+let f = Soru1()
+let gelenSonuc = f.dereceFahrenhiet(derece: 20)
+print("Fahrenhiet: \(gelenSonuc)")
+
+class Soru2{
+    func dikdörtgenCevre(kk:Int, uk:Int){
+       print("Dikdörtgenin Çevresi: \((kk + uk) * 2)")
+    }
+}
+
+
+let d = Soru2()
+d.dikdörtgenCevre(kk: 10, uk: 20)
+
+
+
+class Soru3{
+    
+}
+
+
+class Soru4{
+    
+}
+
+class Soru5{
+    func icAciToplam(n:Int) -> Int {
+        let icAci = (n - 2) * 180
+        return icAci
+        
+    }
+}
+
+let ic = Soru5()
+let gelenSonuc5 = ic.icAciToplam(n: 3)
+print("İç Açılar Toplamı: \(gelenSonuc5)")
+
+
+class Soru6{
+    var saat:Int?
+    var maas:Int?
+    func maasHesabi(gün:Int) -> Int{
+       saat = (gün * 8)
+      if saat! <= 160
+        {
+          let mesaisizMaas = saat! * 10
+          return mesaisizMaas
+      }else if (saat! > 160){
+          let mesailiMaas = ((saat! - 160) * 20) + (160 * 10)
+          return mesailiMaas
+      }
+        return maas!
+    }
+}
+
+let maasHesaplama = Soru6()
+let gelenSonuc6 = maasHesaplama.maasHesabi(gün: 40)
+print("Maaş: \(gelenSonuc6)")
+
+
+class Soru7{
+    var normalKota:Int?
+    var ücret:Int?
+    func ucretHesapla(kota:Int) -> Int {
+        if kota <= 50
+        {
+            let normalKota = 100
+            return normalKota
+        }
+        else if (kota > 50)
+        {
+            let asimliKota = ((kota - 50) * 4) + 100
+            return asimliKota
+        }
+        return ücret!
+    }
+}
+
+let ücretHesaplama = Soru7()
+let gelenSonuc7 = ücretHesaplama.ucretHesapla(kota: 70)
+print("Ücret: \(gelenSonuc7) TL")
+
+
